0133599b656918d4ae99b53478701f0ceaa7dc11
Tashby <tashby2714@gmail.com>
2016-06-22 10:29:08 -0400

    slight changes

--- logic.js
+++ logic.js
@@ -1,13 +1,18 @@
-var token = '2644462b52924f83b1c3914d95cc24a7', // learn how to obtain it below
-    hashtag = 'popular', // User ID - get it in source HTML of your Instagram profile or look at the next example :)
-    num_photos = 4; // how much photos do you want to get
+/* var clientId = '2644462b52924f83b1c3914d95cc24a7',
+    hashTag = 'popular', 
+    num_photos = 4; 
+    endpoint = 'https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=2644462b52924f83b1c3914d95cc24a7',
+ //   endpoint = BASE_URL +'/media/popular?client_id=' +clientId;*/
 
-$.ajax({
+
+    /* "rotation_period": "24",
+    "orbital_period": "4818",
     type: "GET",
     cache: false,
-    url: 'https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=2644462b52924f83b1c3914d95cc24a7',
+    url: endpoint,
+    //dataType:
     data: {
-        access_token: token,
+       // access_token: token,
         count: num_photos
     },
     success: function(data) {
@@ -15,4 +20,11 @@ $.ajax({
             $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
         }
     }
-});
+ */
+
+
+
+$.ajax({
+    url: http://swapi.co/api/planets/,
+    
+    }); 
