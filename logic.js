8e5fe9f1e878d9efa5e4e019cbfcf9fe21ed6a2d
Tashby <tashby2714@gmail.com>
2016-06-24 03:12:03 -0400

    second update

--- logic.js
+++ logic.js
@@ -1,11 +1,11 @@
-/* var clientId = '2644462b52924f83b1c3914d95cc24a7',
+var clientId = '2644462b52924f83b1c3914d95cc24a7',
     hashTag = 'popular', 
     num_photos = 4; 
     endpoint = 'https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=2644462b52924f83b1c3914d95cc24a7',
- //   endpoint = BASE_URL +'/media/popular?client_id=' +clientId;*/
+   endpoint = BASE_URL +'/media/popular?client_id=' +clientId;
 
 
-    /* "rotation_period": "24",
+     "rotation_period": "24",
     "orbital_period": "4818",
     type: "GET",
     cache: false,
@@ -20,7 +20,7 @@
             $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
         }
     }
- */
+ 
 
 
 
